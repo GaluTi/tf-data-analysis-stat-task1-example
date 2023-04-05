98c869aa4c83349ea8046de04f8417f4b637d7ef
J.P <62064427+GaluTi@users.noreply.github.com>
2023-04-05 12:52:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+    	
+chat_id = 640884635 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return (min(x) + 43) / 10 # Ваш ответ
